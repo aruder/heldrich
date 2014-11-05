06ce9251f7d1eb861beb75fd60338f2332f34306
aruder <aruder56@gmail.com>
2014-11-05 10:42:33 -0500

    test commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import sys
+
+def funny(x):
+	# This function returns doubles a number's value
+	x = x + x 
+	test = "This is odd:" + " " + str(x)
+	return(x, test)
+	
+
+	# Standard boilerplate to call the main() function to begin
+# the program.
+	if __name__ == '__main__':
+		main()
+
+
+	
+	
+

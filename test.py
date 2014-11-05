331651f96818f2c15dddc1f6180877b42580fce0
aruder <aruder56@gmail.com>
2014-11-05 11:33:04 -0500

    update

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 import sys
 
-def funny(x):
-	# This function returns doubles a number's value
-	x = x + x 
-	test = "This is odd:" + " " + str(x)
-	return(x, test)
+
+def heldrich(x):
+	# This function simply returns a 'welcome' to the Heldrich Center
+	return("The " + x + " Center at Rutgers")
+	
+	
+def main():
+	# This main function takes an input of the user's name. Example: python newtext.py Bill
+	print 'Welcome to the Heldrich Center repository,', sys.argv[1]
+	print heldrich("Heldrich")
 	
 
-	# Standard boilerplate to call the main() function to begin
-    # the program.
+# Standard boilerplate to call the main() function to begin
+# the program.
 
-if __name__ == '__main__':
-		main()
-
-
+if __name__ == '__main__': 
+	main()
 	
 	
 
